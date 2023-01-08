3cebd8d8a24c0d4929595cc19b44c91c67202df3
kovac.robert <robert.kovac.roko@gmail.com>
2023-01-08 20:42:13 +0100

    liquibase implemented and some initial data inserted

--- src/main/java/com/blog/myblog/security/AuthenticationContext.java
+++ src/main/java/com/blog/myblog/security/AuthenticationContext.java
@@ -1,15 +1,20 @@
 package com.blog.myblog.security;
 
 import com.blog.myblog.domain.AppUser;
+import com.blog.myblog.repositories.UserRepository;
 import org.springframework.stereotype.Component;
+import javax.persistence.EntityNotFoundException;
 
 @Component
 public class AuthenticationContext {
 
-    // this is dummy data for now
-    public AppUser getSignedInUser(){
+    UserRepository userRepository;
 
-        return new AppUser();
+    // hardcoded data until security not implemented
+    public AppUser getSignedInUser(){
+        return userRepository
+                .findById(1L)
+                .orElseThrow(()-> new EntityNotFoundException("Not found"));
     }
 
 
--- src/main/java/com/blog/myblog/services/UserService.java
+++ src/main/java/com/blog/myblog/services/UserService.java
@@ -1,9 +1,13 @@
 package com.blog.myblog.services;
 
+import com.blog.myblog.domain.AppUser;
+import com.blog.myblog.domain.Article;
+import com.blog.myblog.dto.article.ArticleDetailsDTO;
 import com.blog.myblog.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 
 @Service
@@ -12,6 +16,13 @@ import javax.transaction.Transactional;
 public class UserService {
 
     private UserRepository userRepository;
+
+
+
+    private AppUser getSpecificArticleById(Long id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Article not found with id " + id));
+    }
 
 
 }
